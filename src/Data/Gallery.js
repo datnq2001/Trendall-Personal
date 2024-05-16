7f2466d0ea53bfa253ed9b439950efe1a0a78094
datnq2001 <datnq2001@gmail.com>
2024-05-16 17:14:17 +1000

    Fix

---
+++ src/Data/Gallery.js
@@ -0,0 +1,20 @@
+export const photos= [
+    
+            'https://i.pinimg.com/236x/8e/b7/2b/8eb72bc1211f497de2b3c53e702420ea.jpg',
+            'https://i.pinimg.com/236x/b6/17/57/b6175717494285427ad334c1e1ca9e38.jpg',
+            
+            'https://i.pinimg.com/236x/72/70/3b/72703bf25f140c782e17e12c2b310f8b.jpg',
+            'https://i.pinimg.com/236x/1d/1c/0c/1d1c0c342f567c1289a5fdfd0041597b.jpg',
+            'https://i.pinimg.com/236x/3a/b5/3f/3ab53f25bc25ed3f1dcb6fac66d948c4.jpg',
+            'https://i.pinimg.com/236x/75/cd/30/75cd303060877e0952d6e7333a399306.jpg',
+            'https://i.pinimg.com/236x/f6/4c/36/f64c36670d62da4ed3c5cde77cd98762.jpg',
+            'https://i.pinimg.com/236x/b4/f9/71/b4f9716eab1212bcf78166ad9f611fc9.jpg',
+            'https://i.pinimg.com/236x/fd/bc/34/fdbc3452d84a892b172f4fb27af8ffd6.jpg',
+            'https://i.pinimg.com/236x/a3/ad/f9/a3adf9c81bd55ad44b54b17c1016b8fc.jpg',
+            'https://i.pinimg.com/236x/b4/81/20/b481205c0299ea446156ff4d3da4c282.jpg',
+            'https://i.pinimg.com/236x/48/08/62/480862e688c66fcc44b03516c63e3a50.jpg',
+            'https://i.pinimg.com/236x/21/2d/05/212d054d144defce0f970bd317a7a2d4.jpg',
+            'https://i.pinimg.com/236x/8a/57/98/8a5798fc92b5546fd74b081d1deee6e2.jpg',
+            'https://i.pinimg.com/236x/86/c3/a6/86c3a6dc80cfabecd3b1be64736b6d55.jpg',
+            'https://i.pinimg.com/236x/81/46/17/8146179b5abfde4b4679a9cfdec095dd.jpg'
+        ]
